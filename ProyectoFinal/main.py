9ee3faa5b9ff9e605cde1cbce56dc1a8f57072f6
NauzetDP <nauuuni@gmail.com>
2023-12-14 13:17:27 +0000

    Update main.py ProyectoFinal

--- ProyectoFinal/main.py
+++ ProyectoFinal/main.py
@@ -19,20 +19,16 @@ while cap.isOpened():
 
     # Process the frame with Mediapipe hands
     results = hands.process(rgb_frame)
-
+    
     # Check if hands are detected
     if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            # Draw landmarks on the image
-            mp.solutions.drawing_utils.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+        for i in range(len(results.multi_hand_landmarks)):
+            mp.solutions.drawing_utils.draw_landmarks(frame, results.multi_hand_landmarks[i], mp_hands.HAND_CONNECTIONS)
 
-            # Get handedness (left or right)
-            # print(results.multi_handedness[0].classification[0].label)
-            hand_label = "Right" if results.multi_handedness[0].classification[0].label == "Right" else "Left"
+            hand_label = "Right" if results.multi_handedness[i].classification[0].label == "Left" else "Left"
 
-            # Display hand label
             cv2.putText(frame, hand_label, 
-                        (int(hand_landmarks.landmark[0].x * frame.shape[1]), int(hand_landmarks.landmark[0].y * frame.shape[0])),
+                        (int(results.multi_hand_landmarks[i].landmark[0].x * frame.shape[1]), int(results.multi_hand_landmarks[i].landmark[0].y * frame.shape[0])),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
 
     # Display the frame
