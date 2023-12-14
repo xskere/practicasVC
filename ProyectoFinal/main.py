6ed005342a6a699755e345de381f00147036ad79
NauzetDP <nauuuni@gmail.com>
2023-12-14 13:20:40 +0000

    Update main.py ProyectoFinal

--- ProyectoFinal/main.py
+++ ProyectoFinal/main.py
@@ -16,16 +16,18 @@ while cap.isOpened():
 
     # Convert the BGR image to RGB
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    rgb_frame = cv2.flip(rgb_frame, 1)
+    frame = cv2.flip(frame, 1)
 
     # Process the frame with Mediapipe hands
     results = hands.process(rgb_frame)
-    
+
     # Check if hands are detected
     if results.multi_hand_landmarks:
         for i in range(len(results.multi_hand_landmarks)):
             mp.solutions.drawing_utils.draw_landmarks(frame, results.multi_hand_landmarks[i], mp_hands.HAND_CONNECTIONS)
 
-            hand_label = "Right" if results.multi_handedness[i].classification[0].label == "Left" else "Left"
+            hand_label = "Right" if results.multi_handedness[i].classification[0].label == "Right" else "Left"
 
             cv2.putText(frame, hand_label, 
                         (int(results.multi_hand_landmarks[i].landmark[0].x * frame.shape[1]), int(results.multi_hand_landmarks[i].landmark[0].y * frame.shape[0])),
